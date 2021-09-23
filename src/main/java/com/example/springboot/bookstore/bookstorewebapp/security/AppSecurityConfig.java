9a09912dbb2a31fa529630edf5a1e4bd9a387e8e
abhigyan-pandey <abhigyan.pandey@zemosolabs.com>
2021-09-23 12:01:18 +0530

    Changing the password encoder

--- src/main/java/com/example/springboot/bookstore/bookstorewebapp/security/AppSecurityConfig.java
+++ src/main/java/com/example/springboot/bookstore/bookstorewebapp/security/AppSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.example.springboot.bookstore.bookstorewebapp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.sql.DataSource;
 
 @Configuration
@@ -16,10 +18,17 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
 {
     @Autowired
     DataSource dataSource ;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery("select username, password, enabled from users where username=?")
                 .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
     }
